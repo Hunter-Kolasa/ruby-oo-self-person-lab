4f2658c8f29030f75baa1e3105a993a3abb9a17d
Hunter <kolasa.hunter@aol.com>
2020-11-19 11:15:13 -0500

    commit tree cec950bbb1e0f15ba185365701aef6b4bc209b12

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,67 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+    def happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+        @happiness
+    end
+    def hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0 
+            @hygiene = 0
+        end
+        @hygiene
+    end
+    def happy?
+        @happiness > 7 ? true : false
+    end
+    def clean?
+        @hygiene > 7 ? true : false
+    end
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene=(@hygiene + 4)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.hygiene=(@hygiene - 3)
+        self.happiness=(@happiness + 2)
+        return "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        Person.new(friend)
+        self.happiness=(@happiness + 3)
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(person, topic)
+        objects = [self, person]
+        if topic == "politics"
+            objects.each {|obj| obj.happiness -= 2}
+            first, second = ["partisan", "lobbyist"]
+        elsif topic == "weather"
+            objects.each {|obj| obj.happiness += 1}
+            first, second = ["sun", "rain"]
+        end
+        first ||= "blah"
+        second ||= "blah"
+        base_string = "blah blah #{first} blah #{second}"
+
+    end
+end
